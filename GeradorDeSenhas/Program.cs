d4e655e59a397f31df805b0009e06f108b65d731
lucas <lucas@LUCAS-PC>
2024-11-11 20:35:58 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorDeSenhas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+namespace GeradorDeSenhas { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtd = int.Parse(args[0]);
+            string caracteresPossiveis = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%0123456789";
+
+
+            Random sorteio = new Random();
+            int numeroSorteado = 0;
+            StringBuilder password = new StringBuilder();
+
+
+            for (int t = 0; t < qtd; t++) {
+                numeroSorteado = sorteio.Next(0, caracteresPossiveis.Length - 1);
+                password.Append(caracteresPossiveis[numeroSorteado]);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(password);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
